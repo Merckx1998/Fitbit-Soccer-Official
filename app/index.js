96b71cffc3bf5328aad2b8e7de4a4666e09961eb
Peter Graham <pwgraham91@gmail.com>
2018-05-05 11:24:30 -0500

    countdown and vibrate at end

--- app/index.js
+++ app/index.js
@@ -2,7 +2,39 @@
  * Entry point for the watch app
  */
 import document from "document";
+import { vibration } from 'haptics';
 
-let demotext = document.getElementById("demotext");
-// demotext.text = "Fitbit Studio rocks!";
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    const interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text = minutes + ":" + seconds;
+        if (--timer < 0) {
+          clearInterval(interval);
+          display.text = 'END HALF';
+          vibrate(3);
+        }
+    }, 1000);
+}
+
+function vibrate(secondsToVibrate) {
+  vibration.start('bump')
+  
+  let countdown = secondsToVibrate;
+  const vibrateTimer = setInterval(function() {
+    countdown--;
+    if (countdown <= 0) {
+      clearInterval(vibrateTimer);
+      vibration.stop();
+    }
+  })
+}
+
+const gameTimeClock = document.getElementById("game-time-clock")
+startTimer(22 * 60, gameTimeClock);
 
